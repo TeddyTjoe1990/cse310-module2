3911dda5227f531d894368e78ece4fe1ae9f8e72
TeddyTjoe1990 <her21059@byui.edu>
2025-06-09 18:34:37 +0700

    edited some file so that the code run in the Android Studio

--- health_and_fitness_app/lib/main.dart
+++ health_and_fitness_app/lib/main.dart
@@ -19,6 +19,8 @@ void main() => runApp(
     );
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     // Access the current theme mode from provider
